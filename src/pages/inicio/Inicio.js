bde6d04a1e71fce00ee7830e815d2d324ba4dcaf
Vanessa Oliveira <vanessa.cs.oliveira@gmail.com>
2022-04-18 11:52:16 -0300

    adicionando imagem no inicio

--- src/pages/inicio/Inicio.js
+++ src/pages/inicio/Inicio.js
@@ -1,5 +1,6 @@
 import './styles.css'
 import Navbar from './Navbar'
+import img from '../../assets/image-inicio.png'
 
 
 function Inicio() {
@@ -11,6 +12,10 @@ function Inicio() {
             <p className='details'>Encontre aqui a melhor forma de você organizar seu dinheiro, totalmente grátis e prático de usar!
             </p>
             <a href='../cadastro/Cadastro.js' className='cv-btn'>Cadastra-se já!</a>
+        </div>
+
+        <div className='image'>
+          <span className="img-inicio"><img src={img} alt="imagem"></img> </span> 
         </div>
       </div>
   );
