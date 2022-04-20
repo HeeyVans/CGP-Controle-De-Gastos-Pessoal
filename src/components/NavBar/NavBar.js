c360832835d7b7d20358d1ec34ceecf72c495094
Ytalo Paulo <ytalo.ypw@gmail.com>
2022-04-20 11:38:11 -0300

    update navbar
    Estilizando o menu navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,24 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+//Imagens e Icones
 import logo from "../../assets/imagens/logo-Outline.png";
 import btnNotificacoes from '../../assets/imagens/btnNotificacoes.png';
 import btnPerfil from '../../assets/imagens/btnPerfil.png';
 
+//Estilos
+import styles from './NavBar.module.css'
+
 const NavBar = () => {
     return (
-        <nav>
-            <Link to="/">
-                <img src={ logo } alt="logo" />
-            </Link>
-            <Link to="/inicio"> Início </Link>
-            <Link to="/relatorios"> Relatórios </Link>
-            <Link to="/ajustes"> Ajustes </Link>
-            <Link to="/#">
-                <img src={ btnNotificacoes } alt="Notificações" width='50px' height='50px'/>
-            </Link>
-            <Link to="/#">
-                <img src={ btnPerfil } alt="Notificações" width='50px' height='50px'/>
-            </Link>
+        <nav class={styles.navbar}>
+            <div>
+                <Link to="/">
+                    <img src={logo} alt="logo" />
+                </Link>
+            </div>
+            <ul>
+                <li>
+                    <Link to="/inicio"> Início </Link>
+                </li>
+                <li>
+                    <Link to="/relatorios"> Relatórios </Link>
+                </li>
+                <li>
+                    <Link to="/ajustes"> Ajustes </Link>
+                </li>
+            </ul>
+            <div class={styles.menuEsq}>
+                <Link to="/#">
+                    <img src={btnNotificacoes} alt="Notificações" width='50px' height='50px' />
+                </Link>
+                <Link to="/#">
+                    <img src={btnPerfil} alt="Notificações" width='50px' height='50px' />
+                </Link>
+            </div>
         </nav>
     );
 }
